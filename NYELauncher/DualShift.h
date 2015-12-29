d8e281d058109c5c7f01ead54014c07b96755f65
Xasin <logger89@web.de>
2015-12-29 12:52:58 +0100

    First commit for the shift register code.

---
+++ NYELauncher/DualShift.h
@@ -0,0 +1,32 @@
+/*
+ * DualShift.h
+ *
+ *  Created on: Dec 29, 2015
+ *      Author: xasin
+ */
+
+#ifndef DUALSHIFT_H_
+#define DUALSHIFT_H_
+
+#include <avr/io.h>
+
+class DualShift {
+private:
+
+	uint8_t *IOREG;
+	uint8_t PINS;
+
+	void bitOut(uint8_t b);
+	void activate();
+	void shiftOut(uint8_t data);
+
+public:
+	uint8_t REG_A=0;
+	uint8_t REG_B=0;
+
+	DualShift(uint8_t *port, uint8_t pins);
+
+	void update();
+};
+
+#endif /* DUALSHIFT_H_ */
---
+++ NYELauncher/main.cpp
@@ -0,0 +1,10 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Dec 29, 2015
+ *      Author: xasin
+ */
+
+
+
+
